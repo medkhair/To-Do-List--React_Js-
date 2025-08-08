a0aabe93418537718e2dc99ef445c05a2df7ec21
medkhair <pmedkhair@gmail.com>
2025-08-08 22:47:25 +0100

    showing todos

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,10 +1,23 @@
+import {useState} from 'react'
 
 function Todo(){
+    const [todo, setTodo] = useState("");
+    const [todos, setTodos] = useState([]);
+    function handleSubmition(e){
+        e.preventDefault();
+        setTodos([...todos,todo]);
+        setTodo("");
+    }
     return (
-        <form>
-            <input type="text" />
-            <button type="submit">Add</button>
-        </form>
+        <>
+            <form onSubmit={handleSubmition}>
+                <input onChange={(e) => { setTodo(e.target.value) }} type="text" value={todo} />
+                <button type="submit">Add</button>
+
+            </form>
+            {console.log(todos)}
+
+        </>
     );
 
    
---
+++ src/Components/TodoItem.jsx
@@ -0,0 +1,10 @@
+ 
+function TodoItem({item}){
+    return (
+        <>
+            <h3>{item}</h3>
+        </>
+    )
+}
+
+export default TodoItem;
